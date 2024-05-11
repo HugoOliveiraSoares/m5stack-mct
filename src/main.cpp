5c744d2223d4e0f02ddb886b8fbec56b06254b9b
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2024-05-11 17:17:41 -0300

    testing menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,46 @@
 #include "DeviceSelect.h"
 #include "entities/DeviceBase.h"
+#include "interface/Menu.h"
+#include "interface/MenuItem.h"
+#include "interface/ScreenManager.h"
 #include <Arduino.h>
 #include <SPI.h>
 #include <TFT_eSPI.h>
 
-Entities::DeviceBase *dev;
+using namespace Entities;
 
 std::shared_ptr<TFT_eSPI> display;
 
+DeviceBase *dev;
+Interface::ScreenManager screenManager;
+Interface::Menu mainMenu;
+
 void setup()
 {
-    dev = Entities::DeviceBase::getInstance();
+    dev = DeviceBase::getInstance();
 
     dev->begin();
     display = dev->getDisplay();
+
+    mainMenu = Interface::Menu(dev->getDisplay(), dev->getButton());
+
+    Interface::MenuItem readMenuItem("readSubMenu", "Read Tag");
+    Interface::MenuItem writeMenuItem("writeSubMenu", "Write Tag");
+    Interface::MenuItem emulateMenuItem("emulateSubMenu", "Emulate Tag");
+    Interface::MenuItem importMenuItem("importSubMenu", "Import Tag");
+
+    readMenuItem.setOnClick([]() { Serial.println("Botão Pressionado"); });
+
+    writeMenuItem.setOnClick([]() { Serial.println("Botão Pressionado"); });
+
+    mainMenu.addItem(readMenuItem);
+    mainMenu.addItem(writeMenuItem);
+    mainMenu.addItem(emulateMenuItem);
+    mainMenu.addItem(importMenuItem);
+
+    screenManager = Interface::ScreenManager(dev->getDisplay());
+
+    screenManager.setCurrentScreen(&mainMenu, false);
 }
 
 void loop()
@@ -22,10 +49,15 @@ void loop()
 
     if (dev->getButton()->isPressed())
     {
-        display->drawString("Button isPressed", 240 / 2, 135 / 2);
+        screenManager.getCurrentScreen()->nextButtonPressed();
     }
-    if (dev->getButton()->isReleased())
-    {
-        display->fillScreen(TFT_BLACK);
-    }
+
+    // if (dev->getButton()->isPressed())
+    // {
+    //     display->drawString("Button isPressed", 240 / 2, 135 / 2);
+    // }
+    // if (dev->getButton()->isReleased())
+    // {
+    //     display->fillScreen(TFT_BLACK);
+    // }
 }
