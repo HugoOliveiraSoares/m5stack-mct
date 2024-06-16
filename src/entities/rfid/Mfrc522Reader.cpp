574ab95495d7674eedc79f1bd00423dfc3c082f1
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2024-06-16 10:12:37 -0300

    Mfrc522 Reader implementation
    - Constructor
    - isCardPresent()
    - authenticate()
    - readRegister()
    - getUid()
    - stop()
        - put picc in state halt
        - Used to exit the PCD from its authenticated state
        - maybe i change to halt

--- src/entities/rfid/Mfrc522Reader.cpp
+++ src/entities/rfid/Mfrc522Reader.cpp
@@ -1,35 +1,77 @@
 #include "Mfrc522Reader.h"
+#include <cstdint>
 
 using namespace Entities::RFID;
 
 Mfrc522Reader::Mfrc522Reader()
 {
+    this->_mfrc522 = new MFRC522_I2C(0x28, 0);
+    this->_mfrc522->PCD_Init();
 }
 
 bool Mfrc522Reader::isCardPresent()
 {
+    return !this->_mfrc522->PICC_IsNewCardPresent() || !this->_mfrc522->PICC_ReadCardSerial();
 }
 
-bool Mfrc522Reader::authenticate(uint8_t reg, uint8_t key, uint8_t uid)
+bool Mfrc522Reader::authenticate(uint8_t reg, uint8_t *key, uint8_t *uid)
+{
+    MFRC522_I2C::MIFARE_Key _key;
+    for (int i = 0; i < 6; i++)
+    {
+        _key.keyByte[i] = *(key + i);
+    }
+
+    uint8_t status =
+        this->_mfrc522->PCD_Authenticate(MFRC522_I2C::PICC_CMD_MF_AUTH_KEY_A, reg, &_key, &(this->_mfrc522->uid));
+
+    if (status != MFRC522_I2C::STATUS_OK)
+    {
+        Serial.print(F("Authentication failed: "));
+        Serial.println(this->_mfrc522->GetStatusCodeName(status));
+        return 0;
+    }
+    return 1;
+}
+
+uint8_t Mfrc522Reader::dumpMemory(uint8_t *uid)
 {
 }
 
-uint8_t Mfrc522Reader::dumpMemory(uint8_t uid)
+uint8_t *Mfrc522Reader::readSector(uint8_t reg)
 {
 }
 
-uint8_t Mfrc522Reader::readSector(uint8_t reg)
+uint8_t *Mfrc522Reader::readRegister(uint8_t reg)
 {
+    uint8_t len = 18;
+    static uint8_t buffer[18];
+    uint8_t status = this->_mfrc522->MIFARE_Read(reg, buffer, &len);
+
+    if (status != MFRC522_I2C::STATUS_OK)
+    {
+        Serial.print(F("Reading failed: "));
+        Serial.println(this->_mfrc522->GetStatusCodeName(status));
+        return nullptr;
+    }
+    return buffer;
 }
 
-uint8_t Mfrc522Reader::readRegister(uint8_t reg)
-{
-}
-
-bool Mfrc522Reader::writeRegister(uint8_t reg, uint8_t value)
+bool Mfrc522Reader::writeRegister(uint8_t reg, uint8_t *value, uint8_t valueSize)
 {
 }
 
 bool Mfrc522Reader::setUid(uint8_t *newUid)
 {
+}
+
+uint8_t *Mfrc522Reader::getUid()
+{
+    return this->_mfrc522->uid.uidByte;
+}
+
+void Mfrc522Reader::stop()
+{
+    this->_mfrc522->PICC_HaltA();
+    this->_mfrc522->PCD_StopCrypto1();
 }
--- src/entities/rfid/Mfrc522Reader.h
+++ src/entities/rfid/Mfrc522Reader.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include "MFRC522_I2C.h"
 #include "RfidInterface.h"
 #include <WString.h>
+#include <cstdint>
 
 namespace Entities::RFID
 {
@@ -10,14 +12,17 @@ class Mfrc522Reader : public RfidModuleInterface
   public:
     Mfrc522Reader();
     bool isCardPresent();
-    bool authenticate(uint8_t reg, uint8_t key, uint8_t uid);
-    uint8_t dumpMemory(uint8_t uid);
-    uint8_t readSector(uint8_t reg);
-    uint8_t readRegister(uint8_t reg);
-    bool writeRegister(uint8_t reg, uint8_t value);
+    bool authenticate(uint8_t reg, uint8_t *key, uint8_t *uid);
+    uint8_t dumpMemory(uint8_t *uid);
+    uint8_t *readSector(uint8_t reg);
+    uint8_t *readRegister(uint8_t reg);
+    bool writeRegister(uint8_t reg, uint8_t *value, uint8_t valueSize);
     bool setUid(uint8_t *newUid);
+    uint8_t *getUid();
+    void stop();
 
   private:
+    MFRC522_I2C *_mfrc522;
 };
 
 } // namespace Entities::RFID
