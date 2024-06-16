40e0d0f0ebf6b4417e711acc7921be55d61bda99
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2024-06-16 10:03:13 -0300

    fix types

--- src/entities/rfid/RfidInterface.h
+++ src/entities/rfid/RfidInterface.h
@@ -12,12 +12,13 @@ class RfidModuleInterface
     {
     }
     virtual bool isCardPresent() = 0;
-    virtual bool authenticate(uint8_t reg, uint8_t key, uint8_t uid) = 0;
-    virtual uint8_t dumpMemory(uint8_t uid) = 0;
-    virtual uint8_t readSector(uint8_t reg) = 0;
-    virtual uint8_t readRegister(uint8_t reg) = 0;
-    virtual bool writeRegister(uint8_t reg, uint8_t value) = 0;
+    virtual bool authenticate(uint8_t reg, uint8_t *key, uint8_t *uid) = 0;
+    virtual uint8_t dumpMemory(uint8_t *uid) = 0;
+    virtual uint8_t *readSector(uint8_t reg) = 0;
+    virtual uint8_t *readRegister(uint8_t reg) = 0;
+    virtual bool writeRegister(uint8_t reg, uint8_t *value, uint8_t valueSize) = 0;
     virtual bool setUid(uint8_t *newUid) = 0;
+    virtual uint8_t *getUid() = 0;
 
   private:
 };
