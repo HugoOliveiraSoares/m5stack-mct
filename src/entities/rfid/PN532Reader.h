9c58f137912318d3825ae4558a5918a914ff69c4
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2024-06-20 10:02:15 -0300

    adds correct methods of RfidModuleInterface

--- src/entities/rfid/PN532Reader.h
+++ src/entities/rfid/PN532Reader.h
@@ -12,11 +12,14 @@ class PN532Reader : public RfidModuleInterface
 {
   public:
     PN532Reader();
-    void dumpMemory();
-    void readSector(byte reg);
-    void readRegister(byte reg);
-    void writeRegister(byte reg, byte value);
-    void setUid(byte *newUid);
+    bool isCardPresent();
+    bool authenticate(uint8_t reg, uint8_t *key, uint8_t *uid);
+    uint8_t *dumpMemory(uint8_t *uid, uint8_t *key);
+    uint8_t *readSector(uint8_t sec);
+    uint8_t *readRegister(uint8_t reg);
+    bool writeRegister(uint8_t reg, uint8_t *value, uint8_t valueSize);
+    bool setUid(uint8_t *newUid);
+    uint8_t *getUid();
 
   private:
     PN532_I2C *_pn532i2c;
